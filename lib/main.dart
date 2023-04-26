b824c7f125ff0567e06278e238e52a4024319329
Arun <arunislordofgod49@gmail.com>
2023-04-26 18:29:59 +0530

    Chnage branch and play with it

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final PageController _pageController = PageController(initialPage: 0);
   int _counter = 0;
 
   void _incrementCounter() {
@@ -36,25 +37,56 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<Widget> _buildThreePageViewChildren() {
+    return <Widget>[
+      Container(color: Colors.red),
+      Container(color: Colors.blue),
+      Container(color: Colors.green),
+    ];
+  }
+
+  List<BottomNavigationBarItem> _buildThreeItems() {
+    return const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.heart_broken),
+        label: 'Likes',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: 'Setting',
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: PageView(children: _buildThreePageViewChildren()
+          //  Center(
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: <Widget>[
+          //       const Text(
+          //         'You have pushed the button this many times:',
+          //       ),
+          //       Text(
+          //         '$_counter',
+          //         style: Theme.of(context).textTheme.headlineMedium,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _buildThreeItems(),
+        onTap: (value) {},
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
