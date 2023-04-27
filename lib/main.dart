6b44c32a4788d895a3d01d6f91e61a43fec58bde
Arun <arunislordofgod49@gmail.com>
2023-04-27 11:10:15 +0530

    Init for provider library

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:bloc/bloc.dart';
-import 'package:fluter_workout/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 
-import 'app.dart';
-
 void main(List<String> args) {
-  Bloc.observer = const SimpleBlocObserver();
-  runApp(const App());
+  runApp(const Center());
 }
