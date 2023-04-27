1541e88e8dbe9aec3db1f1c58525533be6cf3520
Arun <arunislordofgod49@gmail.com>
2023-04-27 10:45:05 +0530

    Add: bloc flutter library

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:fluter_workout/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'app.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(title: 'Flutter Test'));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final PageController _pageController = PageController(initialPage: 0);
-  int _pageIndex = 0;
-
-  List<Widget> _buildThreePageViewChildren() {
-    return <Widget>[
-      Container(color: Colors.blue),
-      Container(color: Colors.red),
-      Container(color: Colors.green),
-    ];
-  }
-
-  List<BottomNavigationBarItem> _buildThreeItems() {
-    return const <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: 'Home',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.heart_broken),
-        label: 'Likes',
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.settings),
-        label: 'Setting',
-      ),
-    ];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: PageView(
-        controller: _pageController,
-        physics: const NeverScrollableScrollPhysics(),
-        onPageChanged: (value) {
-          setState(() {
-            _pageIndex = value;
-          });
-        },
-        children: _buildThreePageViewChildren(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: _buildThreeItems(),
-        currentIndex: _pageIndex,
-        onTap: (value) {
-          print(value);
-          _pageController.animateToPage(
-            value,
-            duration: const Duration(milliseconds: 100),
-            curve: Curves.easeInOut,
-          );
-        },
-      ),
-    );
-  }
+void main(List<String> args) {
+  Bloc.observer = const SimpleBlocObserver();
+  runApp(const App());
 }
