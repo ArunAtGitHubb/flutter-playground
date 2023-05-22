68a72907b791d26282690ae7cbd81f5b75203ad3
Arun <arunislordofgod49@gmail.com>
2023-05-22 17:07:09 +0530

    Commenting Firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluter_workout/data/joke_repo.dart';
 import 'package:fluter_workout/provider/joke.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'app.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+
+  // await FirebaseAuth.instance.useAuthEmulator("localhost", 9099);
+
   runApp(MyApp());
 }
 
