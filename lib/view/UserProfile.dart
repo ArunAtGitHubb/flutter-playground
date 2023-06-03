38d2fabeafb0d844aaceb216c18b0733977d77dd
Arun <arunislordofgod49@gmail.com>
2023-06-03 11:28:11 +0530

    update profile UI

--- lib/view/UserProfile.dart
+++ lib/view/UserProfile.dart
@@ -1,6 +1,5 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fluter_workout/data/user/user.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class UserProfile extends StatelessWidget {
   UserJson userJson;
@@ -10,29 +9,113 @@ class UserProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     var name =
         "${userJson.results![0].name!.first.toString()} ${userJson.results![0].name!.last.toString()}";
-    var email = userJson.results![0].email.toString();
-    var phone = userJson.results![0].phone.toString();
-    DateTime? dob = userJson.results![0].dob?.date;
-    return Container(
-      child: Column(
-        children: [
-          CachedNetworkImage(
-            imageUrl: userJson.results![0].picture!.medium.toString(),
-            progressIndicatorBuilder: (context, url, downloadProgress) =>
-                CircularProgressIndicator(value: downloadProgress.progress),
-            errorWidget: (context, url, error) =>
-                Icon(Icons.error_outline_outlined),
-          ),
-          SizedBox(height: 10),
-          Text("Name: $name"),
-          SizedBox(height: 10),
-          Text("Email: $email"),
-          SizedBox(height: 10),
-          Text("DOB: ${dob?.day}/${dob?.month}/${dob?.year}"),
-          SizedBox(height: 10),
-          Text("Phone: $phone"),
-          SizedBox(height: 10),
-        ],
+    var imageUrl = userJson.results![0].picture?.large;
+    var email = userJson.results![0].email;
+    var dob = userJson.results![0].dob;
+    var location = userJson.results![0].location;
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Neumorphic(
+                style: NeumorphicStyle(
+                  shape: NeumorphicShape.concave,
+                  boxShape: NeumorphicBoxShape.circle(),
+                  depth: 8,
+                  intensity: 1,
+                  color: Colors.grey[200],
+                ),
+                child: Container(
+                  width: 120,
+                  height: 120,
+                  child: ClipOval(
+                    child: Image.network(
+                      imageUrl!,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              name,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Software Developer',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Email',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              email!,
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Date of Birth',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              dob!.date.toString(),
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Location',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              location!.state!,
+              style: TextStyle(
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
